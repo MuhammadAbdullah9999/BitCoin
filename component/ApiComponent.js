dd88ab598eab5092cd17886ec00bc7584b1e817c
MuhammadAbdullah9999 <hafizzabdullah999@gmail.com>
2023-10-25 21:33:40 +0500

    added useState

--- component/ApiComponent.js
+++ component/ApiComponent.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { Text, View } from "react-native";
+import { useState } from "react";
 
 const ApiComponent = () => {
+	const [data, setData] = useState(''); // Initial empty array of data
 	useEffect(() => {
 		// Define the API endpoint
 		const apiUrl = "https://api.coindesk.com/v1/bpi/currentprice.json"; // Example API
@@ -11,6 +13,7 @@ const ApiComponent = () => {
 			.get(apiUrl)
 			.then((response) => {
 				console.log("API Response:", response.data);
+				setData(response.data); // Set the data in the state
 			})
 			.catch((error) => {
 				console.error("API Error:", error);
